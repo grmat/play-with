80c88abca8167bab61069999d2408779b707e5a3
grmat <grmat@sub.red>
2018-05-22 15:59:34 +0200

    add context-menu item

--- src/background.js
+++ src/background.js
@@ -2,12 +2,34 @@
  * Hand the URL of the current tab over to the content script.
  * TODO: user feedback for 'bad' URLs
  */
-function invoke(tab) {
-  // console.log(tab.url);
-  browser.tabs.sendMessage(tab.id, {url: tab.url});
+function invoke(info, tab) {
+  var url = tab.url;
+  if (info) {
+    url = info.linkUrl;
+  }
+  console.log("play-with attempting to play url " + url);
+  browser.tabs.sendMessage(tab.id, {url: url});
 }
+
+/*
+ * Create the context menu for links.
+ */
+browser.contextMenus.create({
+  id: "play-with",
+  title: browser.i18n.getMessage("actionName"),
+  contexts: ["all"]
+});
 
 /*
  * Register listener for the button.
  */
-browser.browserAction.onClicked.addListener(invoke);
+browser.browserAction.onClicked.addListener((tab) => {
+  invoke(null, tab);
+});
+
+/*
+ * Register listener for the context menu.
+ */
+browser.contextMenus.onClicked.addListener((info, tab) => {
+  invoke(info, tab);
+});
