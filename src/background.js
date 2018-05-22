7e056273f950f177fbdfa79375d3fe27fb68deb3
grmat <grmat@sub.red>
2018-05-22 14:57:44 +0200

    fix permissions

--- src/background.js
+++ src/background.js
@@ -1,29 +1,13 @@
-
 /*
  * Hand the URL of the current tab over to the content script.
+ * TODO: user feedback for 'bad' URLs
  */
-function onSuccess(tabs) {
-  var tab = tabs[0];
-//   console.log(tab.url);
+function invoke(tab) {
+  // console.log(tab.url);
   browser.tabs.sendMessage(tab.id, {url: tab.url});
-}
-
-/*
- * We didn't get the URL of the current tab.
- * TODO user feedback
- */
-function onError(error) {
-  console.log(`Error: ${error}`);
-}
-
-/*
- * Try to get the URL of the current tab.
- */
-function checkTab() {
-  browser.tabs.query({currentWindow: true, active: true}).then(onSuccess, onError);
 }
 
 /*
  * Register listener for the button.
  */
-browser.browserAction.onClicked.addListener(checkTab);
+browser.browserAction.onClicked.addListener(invoke);
