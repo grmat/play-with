fd2284b60dd8d5fb342250d8a7c2db20f9c15f8d
grmat <grmat@sub.red>
2018-06-01 00:18:05 +0200

    add workaround for #1, release v1.3.2

--- src/play.js
+++ src/play.js
@@ -1,13 +1,37 @@
 /*
+ * Workaround for issue #1/upstream bug 1465458
+ */
+var firstRunElapsed;
+
+function onError(error) {
+  console.log(`Error: ${error}`);
+  firstRunElapsed = false;
+}
+
+function onGot(item) {
+  firstRunElapsed = item.firstRunElapsed || false;
+}
+
+var getting = browser.storage.local.get(["firstRunElapsed"]);
+getting.then(onGot, onError);
+
+/*
  * Create a M3U playlist file, paste the passed URL into it and download it.
  */
 function download(request, sender, sendResponse) {
-  var filename = "stream.m3u";
   var content = "#EXTM3U\n" + request.url;
   var element = document.createElement('a');
   var mime = 'video/x-mpegurl';
-  // this prevents FF from remembering to "always open with..."
-  //element.setAttribute('download', filename);
+
+  if (!firstRunElapsed) {
+    // this prevents FF from remembering to "always open with..."
+    var filename = "stream.m3u";
+    element.setAttribute('download', filename);
+    browser.storage.local.set({
+      firstRunElapsed: true
+    });
+  }
+
   element.setAttribute('href', 'data:' + mime + ';charset=utf-8,' + encodeURIComponent(content));
   element.style.display = 'none';
   document.body.appendChild(element);
